c133bc36499761ef72e9e7ea7dfd42b8dc20977d
Vivek r <ramdurgvivek630@gmail.com>
2025-08-13 17:42:52 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ userModel.createTables()
 app.use('/api', authRoutes)
 app.use('/api/user-data', userDataRoutes)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Portfolio server running at http://0.0.0.0:${PORT}`);
+});
