47a5603013338dc586cb29e576ddec5ad66312dd
Vivekanand ramdurg <ramdurgvivek630@gmail.com>
2025-08-17 20:44:06 +0530

    th

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,7 +25,8 @@ function Login() {
 
   return (
     <div className="login-container">
-      <h2 className="form-title">Welcome Back Home</h2>
+      <h2 className="form-title">Welcome Back Home </h2>
+      <h2>This is Vishal and Vivek</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username</label>
